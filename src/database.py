7948ba90d36f3419d98c878ff83e12457b5a28a4
Felipe Vieira <felipedc.dev@gmail.com>
2026-03-24 12:56:13 -0300

    add postgresql support

--- src/database.py
+++ src/database.py
@@ -4,10 +4,12 @@ import sqlalchemy
 import databases
 
 
-
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
 
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
-engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 
+if DATABASE_URL.startswith("sqlite"):
+    engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+else:
+    engine = sqlalchemy.create_engine(DATABASE_URL)
