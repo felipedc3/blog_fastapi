929e84022e1ad595b566a1ce3f4e68018d358503
Felipe Vieira <felipedc.dev@gmail.com>
2026-03-24 14:35:21 -0300

    updating datetime error

--- src/models/post.py
+++ src/models/post.py
@@ -7,7 +7,7 @@ posts = sqlalchemy.Table(
     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column('title', sqlalchemy.String(150), nullable=False, unique=True),
     sqlalchemy.Column('content', sqlalchemy.String, nullable=False),
-    sqlalchemy.Column('published_at', sqlalchemy.DateTime, nullable=True),
+    sqlalchemy.Column('published_at', sqlalchemy.TIMESTAMP(timezone=True), nullable=True),
     sqlalchemy.Column('published', sqlalchemy.Boolean, default=False),
     
 )
--- src/schemas/post.py
+++ src/schemas/post.py
@@ -1,15 +1,14 @@
-from pydantic import BaseModel
-from datetime import datetime, UTC
+from pydantic import BaseModel, AwareDatetime
 
 class PostIn(BaseModel):
     title: str
     content: str
-    published_at: datetime | None = None
+    published_at: AwareDatetime | None = None
     published: bool = False
 
 
 class PostUpdateIn(BaseModel):
     title: str | None = None
     content: str | None = None
-    published_at: datetime | None = None
+    published_at: AwareDatetime | None = None
     published: bool | None = None
